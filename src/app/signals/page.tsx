7c672972494833e32ca2647fb03f8eb501bfd669
anguzuDaniel <anguzudaniel2@gmail.com>
2025-09-24 12:20:52 +0300

    add sub pages

---
+++ src/app/signals/page.tsx
@@ -0,0 +1,9 @@
+import AboutSection from "@/app/components/AboutSection";
+
+export default function Resources() {
+    return (
+        <section className="w-100">
+            <p>Resource page to be added</p>
+        </section>
+    );
+}
